6a455c5333e5dc47cbc91aa7976b85eb7d91f6e3
mahmoud.fatene <mahmoud.fatene@9382a591-7640-0410-a426-b18cb8f8931e>
2007-12-18 12:14:15 +0000

    Show the variation of pressure drop throughout the optimisation

--- matlab/computeV.m
+++ matlab/computeV.m
@@ -1,12 +1,15 @@
-function v = computeV( epsilon, L, D, prssDrop, rho, nu, v0 )
+function [v, swamee ] = computeV( epsilon, L, D, prssDrop, rho, nu, v0 )
 %COMPUTEV Summary of this function goes here
 %   Detailed explanation goes here
 swameeJain = '(';
 for i = 1:size(D,2)
     swameeJain = strcat(swameeJain,'+', num2str(L(i)./D(i)),'*0.25./(log10(',num2str(epsilon(i)),'./(3.7*',num2str(D(i)),')+5.74./((v.*',num2str(D(i)./nu),')^(0.9))).^2)');
 end
-expr = strcat(swameeJain,'+',num2str(Ke(D(1),D(2))+ Kc(D(2),D(3),pi)),')','.*',num2str(rho),'.*v^2./2',num2str(-prssDrop));
+swameeJain = strcat(swameeJain,'+',num2str(Ke(D(1),D(2))+ Kc(D(2),D(3),pi)),')');
+expr = strcat(swameeJain,'.*',num2str(rho),'.*v^2./2',num2str(-prssDrop));
+% expr = strcat(swameeJain,'+',num2str(Ke(D(1),D(2))+ Kc(D(2),D(3),pi)),')','.*',num2str(rho),'.*v^2./2',num2str(-prssDrop));
 % swameeJain = strcat('0.25./(log10(',num2str(epsilon),'./(3.7*',num2str(D),')+5.74./((v.*',num2str(D./nu),')^(0.9))).^2)');
 % expr = strcat('(sum(',swameeJain,'.*',num2str(L./D),')+',num2str(Ke(D(1),D(2))+ Kc(D(2),D(3),pi)),')','.*',num2str(rho),'.*v^2./2',num2str(-prssDrop));
 time_fun=inline(expr,'v');
 v=fsolve(time_fun,v0);
+swamee = eval(swameeJain);
--- matlab/optimiseTime.m
+++ matlab/optimiseTime.m
@@ -2,11 +2,13 @@ Pin = 3*0.98692;
 Q = 2e-3/60;
 L=[20*1e-3, 118*1e-3, 50*1e-3];
 D=[4*1e-3 , 10*1e-3 , 4*1e-3];
-x0 = [D';L(2);Q;Pin];
+x0 = [D';L(2);Pin];
 % 
 % [x,fval,exitflag,output] = fminsearch(@computeTime,x0)
 % 
 Pmin = 2*0.98692;
-lb = [0.001;0.001;0.001;1e-3;3.3333e-5;Pmin];
-ub = [0.008;0.008;0.008;5e-1;6.6667e-5;2*Pmin];
-[x,t,exitflag,output] = fmincon(@computeTime,x0,[],[],[],[],lb,ub);
+% lb = [0.001;0.001;0.001;1e-3;2*Q;Pmin];
+% ub = [0.008;0.008;0.008;5e-1;4*Q;2*Pmin];
+lb = [0.001;0.001;0.001;1e-3;Pmin];
+ub = [0.008;0.008;0.008;5e-1;2*Pmin];
+[x,t,exitflag,output] = fmincon(@computeTime,x0,[],[],[],[],lb,ub)

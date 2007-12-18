0c5195abe4751a69a090e61c90b933c6b3f9dcb9
mahmoud.fatene <mahmoud.fatene@9382a591-7640-0410-a426-b18cb8f8931e>
2007-12-18 07:35:56 +0000

    files modified so as there was a mistake in the way of computing velocity. computeV.m: define the expression involving the velocity and then compute it solving the problem f(x) = 0.

--- trunk/matlab/optimiseTime.m
+++ trunk/matlab/optimiseTime.m
@@ -1,5 +1,5 @@
 Pin = 3*0.98692;
-Q = 2e-2/60;
+Q = 2e-3/60;
 L=[20*1e-3, 118*1e-3, 50*1e-3];
 D=[4*1e-3 , 10*1e-3 , 4*1e-3];
 x0 = [D';L(2);Q;Pin];
@@ -7,6 +7,6 @@ x0 = [D';L(2);Q;Pin];
 % [x,fval,exitflag,output] = fminsearch(@computeTime,x0)
 % 
 Pmin = 2*0.98692;
-lb = [0.001;0.001;0.001;1e-3;Q;Pmin];
-ub = [0.008;0.008;0.008;5e-1;1e-2;2*Pmin];
-[x,t,exitflag,output] = fmincon(@computeTime,x0,[],[],[],[],lb,ub,[],optimset('GradObj','on'))
+lb = [0.001;0.001;0.001;1e-3;3.3333e-5;Pmin];
+ub = [0.008;0.008;0.008;5e-1;6.6667e-5;2*Pmin];
+[x,t,exitflag,output] = fmincon(@computeTime,x0,[],[],[],[],lb,ub);
